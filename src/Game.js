41972bf9712901eb9668771d6dfaff8fef73ca16
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-08-30 10:52:20 +0200

    cleanup

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,6 @@
 import Card from "./Card.js";
 import "./Game.css";
 
-//import './Game.css';
-// var link = document.createElement("link");
-// link.rel = "stylesheet";
-// link.type = "text/css";
-// link.href = "Game.css";
-// document.getElementsByTagName("HEAD")[0].appendChild(link);
-
 export default class Game {
   constructor({ lifeNumber = 10, handleLifeNumber }) {
     this._handleLifeNumber = handleLifeNumber;
