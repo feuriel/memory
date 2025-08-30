f239a18d9373e8e66709698e34a1ba99fe762cef
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-08-30 15:14:46 +0200

    with vite as dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
   build: {
     rollupOptions: {
       output: {
-        // This ensures CSS is properly bundled
         manualChunks: undefined,
         inlineDynamicImports: false,
       },
